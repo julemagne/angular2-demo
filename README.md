f52d7d588807b01b80d2974ee6edab0f33987056
Julie David <julie.angela.david@gmail.com>
2016-06-29 13:11:14 -0400

    Upgrade to Angular 2.0.0-rc.3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Angular 2 demo app :D
+
+Upgraded to Angular 2.0.0-rc.3
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from 'angular2/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: '<div class="container"><h1 class="alert alert-success" style="color:#555555;text-align:center;">My First Angular 2 App!</h1></div>'
 })
 export class AppComponent { }
